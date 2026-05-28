6086a7125eae6a8356db77d2b7e7be8910c54788
adityanath912529-pixel <adityanath912529@gmail.com>
2026-05-28 10:25:05 +0530

    Initial commit

---
+++ Sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int sum=0;
+
+    cout<<"Enter number:";
+     cin>>num;
+
+     for(int i=1;i<=num;i++){ 
+        
+         sum=sum+i; 
+        }
+        cout<<"sum = "<<sum;
+         
+        
+
+}
